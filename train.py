ce2c95f009451012a0b470e4759fa80ee52fd428
Boris Konstantinovskiy <bokonstantinovskiy@avito.ru>
2024-04-27 22:54:33 +0900

    add validation step

--- train.py
+++ train.py
@@ -26,6 +26,7 @@ with open("./config.yaml", "r") as ymlfile:
 
 # Take dataset path
 train_dataset_path = config_file["DATASET"]["TRAIN_PATH"]
+val_dataset_path = config_file["DATASET"].get("VAL_PATH")
 # Load SAM model
 sam = build_sam_vit_b(checkpoint=config_file["SAM"]["CHECKPOINT"])
 #Create SAM LoRA
@@ -34,14 +35,19 @@ model = sam_lora.sam
 # Process the dataset
 processor = Samprocessor(model)
 train_ds = DatasetSegmentation(config_file, processor, mode="train")
+
+if val_dataset_path:
+  val_ds = DatasetSegmentation(config_file, processor, mode="val")
 # Create a dataloader
 train_dataloader = DataLoader(train_ds, batch_size=config_file["TRAIN"]["BATCH_SIZE"], shuffle=True, collate_fn=collate_fn)
+if val_dataset_path:
+  val_dataloader = DataLoader(val_ds, batch_size=config_file["TRAIN"]["BATCH_SIZE"], shuffle=True, collate_fn=collate_fn)
 # Initialize optimize and Loss
 optimizer = Adam(model.image_encoder.parameters(), lr=config_file["TRAIN"]["LEARNING_RATE"], weight_decay=0)
 seg_loss = monai.losses.DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
 num_epochs = config_file["TRAIN"]["NUM_EPOCHS"]
 
-threshold_iou = config_file["TRAIN"]["THR_IOU"]
+threshold_iou = config_file["TRAIN"].get("THR_IOU", 0.5)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 # Set model to train and into the device
@@ -49,11 +55,13 @@ model.train()
 model.to(device)
 
 total_loss = []
-iou_metrics = []
+iou_metrics_train = []
+iou_metrics_val = []
 
 for epoch in range(num_epochs):
     epoch_losses = []
 
+    model.train()
     for i, batch in enumerate(tqdm(train_dataloader)):
       
       outputs = model(batched_input=batch,
@@ -72,11 +80,28 @@ for epoch in range(num_epochs):
 
       # эти строчки я добавил
       iou = calculate_iou(stk_out, stk_gt.int().to(device), thr=threshold_iou)
-      iou_metrics.append(iou)
+      iou_metrics_train.append(iou)
+    
+    # эти строчки я добавил
+    if val_dataset_path:
+      model.eval()
+      for i, batch in enumerate(tqdm(val_dataloader)):
+        with torch.no_grad():
+          outputs = model(batched_input=batch,
+                          multimask_output=False)
+
+          stk_gt, stk_out = utils.stacking_batch(batch, outputs)
+          stk_out = stk_out.squeeze(1)
+          stk_gt = stk_gt.unsqueeze(1) # We need to get the [B, C, H, W] starting from [H, W]
+
+          # эти строчки я добавил
+          iou = calculate_iou(stk_out, stk_gt.int().to(device), thr=threshold_iou)
+          iou_metrics_val.append(iou)
 
     print(f'EPOCH: {epoch}')
     print(f'Mean loss training: {mean(epoch_losses)}')
-    print(f'IoU Train: {mean(iou_metrics)}')
+    print(f'IoU Train: {mean(iou_metrics_train)}')
+    print(f'IoU Val: {mean(iou_metrics_val)}')
 
 # Save the parameters of the model in safetensors format
 rank = config_file["SAM"]["RANK"]
